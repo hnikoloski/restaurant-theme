a017bef95720757da7341bd6c64ede18747a87a4
hnikoloski <h.nikoloski@outlook.com>
2022-01-31 22:59:58 +0100

    theme default setup

--- header.php
+++ header.php
@@ -26,33 +26,15 @@
     <?php wp_body_open(); ?>
     <div id="page" class="site">
         <header id="masthead" class="site-header">
-            <div class="site-branding">
-                <?php
-                the_custom_logo();
-                if (is_front_page() && is_home()) :
-                ?>
-                    <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
-                <?php
-                else :
-                ?>
-                    <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
-                <?php
-                endif;
-                $starter_description = get_bloginfo('description', 'display');
-                if ($starter_description || is_customize_preview()) :
-                ?>
-                    <p class="site-description"><?php echo $starter_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped 
-                                                ?></p>
-                <?php endif; ?>
-            </div><!-- .site-branding -->
+            <a href="<?= get_home_url(); ?>" class="logo-wrapper"><img src="" alt="" class="full-size-img"></a>
 
             <nav id="site-navigation" class="main-navigation">
-                <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'starter'); ?></button>
                 <?php
                 wp_nav_menu(
                     array(
                         'theme_location' => 'menu-1',
                         'menu_id'        => 'primary-menu',
+                        'container'      => false
                     )
                 );
                 ?>
