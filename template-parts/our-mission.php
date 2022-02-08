1b090ec0bc06a970315fc67647dd7697f1db8eb5
hnikoloski <h.nikoloski@outlook.com>
2022-02-08 22:01:41 +0100

    update

--- template-parts/our-mission.php
+++ template-parts/our-mission.php
@@ -1,10 +1,13 @@
 <section id="home-our-mission">
 	<div>
 		<h3 class="animated text-animation add-active fadeInUp">
-			<?php the_field('our_mission_title'); ?>
+			<?php the_field('mission_title'); ?>
 		</h3>
 		<div class="row-content">
+			<div class="row row-spaced animated fadeInUp" data-delay="400">
 
+				<?php the_field('mission_content'); ?>
+			</div>
 		</div>
 	</div>
 </section>
